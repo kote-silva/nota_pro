0ab6c25e162152c9b693b82ab62d8508a7ff9aa7
Kote <cotesilvadiaz5@gmail.com>
2023-06-13 19:25:11 -0400

    Correji la formula para que funcionara correctamente

--- funciones.py
+++ funciones.py
@@ -1,6 +1,11 @@
 def primo(num):
-    for i in range(2, (num**0.5)+1):
+    for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             print("El numero", num, "no es primo")
-        else:
-            print("El numero", num, "es primo")
+            return
+
+    print("El numero", num, "es primo")
+
+
+a = int(input("Agrega el numero que deseas comprobar --> "))
+primo(a)
