45b310ff65132c4b014f570235607f44300bf55b
Kote <cotesilvadiaz5@gmail.com>
2023-06-13 20:03:28 -0400

    Termine de modificar la segunda funcion para que funcioanra correctamente y no me imprimiera todos los numeros anteriores y acaso eran primos o no

--- funciones.py
+++ funciones.py
@@ -1,22 +1,25 @@
 def primo(num):
     for i in range(2, int(num**0.5)+1):
         if num % i == 0:
-            print("El numero", num, "no es primo")
-            return
-
-    x = print("El numero", num, "es primo")
-
+            return True
+    return False
 
 
 def primos(num):
     primos=[]
-    for i in range(0, num+1):
-        if primo(i):
+    for i in range(2, num+1):
+        if primo(i) == False:
             primos.append(i)
     print(primos)
     return primos
 
             
 a = int(input("Agrega el numero que deseas comprobar --> "))
-primo(a)
-primos(a)
+if primo(a) == False:
+    print("El numero", a, "es primo")
+else:
+    print("El numero", a, "no es primo")
+    
+d = input("¿Deseas ver los numero primos anteriores?")
+if d == "si" or d == "Si":
+    primos(a)
